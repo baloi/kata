f9cbe021c5956b72afc514ac418370d489ffa609
baloi <baloi.galia@gmail.com>
2015-09-03 01:02:56 -0400

    cat kata done

--- cat.c
+++ cat.c
@@ -3,5 +3,14 @@
 #define SIZE 512
 
 int main( int argc, char **argv ) {
+    char buf[ SIZE ];
+    int n;
+
+    while ( ( n = read( 0, buf, sizeof( buf ) ) ) > 0 ) {
+
+        write( 1, buf, n );
+
+    } 
+
     return 0;
 }
